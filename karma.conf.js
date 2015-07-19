893c58b5bdb5f2ca85060a9a8d8db22542b85b4b
Adam Poczatek <hello.adaz@gmail.com>
2015-07-19 19:15:22 +0100

    Added more browsers and changed test command

--- karma.conf.js
+++ karma.conf.js
@@ -8,14 +8,82 @@ module.exports = function(config) {
   // Browsers to run on Sauce Labs
   // Check out https://saucelabs.com/platforms for all browser/OS combos
   var customLaunchers = {
-    'SL_Chrome': {
+    'SL_Chrome_Win': {
       base: 'SauceLabs',
-      browserName: 'chrome'
+      browserName: 'chrome',
+      platform: 'Windows 8.1'
     },
-    'SL_Firefox': {
+    'SL_Chrome_Mac': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Linux'
+    },
+    'SL_Chrome_Lin': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X 10.10'
+    },
+
+    'SL_Firefox_Win': {
       base: 'SauceLabs',
       browserName: 'firefox',
-      version: '26'
+      platform: 'Windows 8.1'
+    },
+    'SL_Firefox_Mac': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'Linux'
+    },
+    'SL_Firefox_Lin': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'OS X 10.10'
+    },
+
+    'SL_IE_Win': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    'SL_IE_Mac': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Linux',
+      version: '11'
+    },
+    'SL_IE_Lin': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'OS X 10.10',
+      version: '11'
+    },
+
+    'SL_IE_10_Win': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8',
+      version: '10'
+    },
+
+    'SL_IE_9_Win': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '9'
+    },
+
+    'SL_Iphone': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.10'
+    },
+
+    'SL_Android': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.2'
     }
   };
 
@@ -31,7 +99,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     sauceLabs: {
-      testName: 'Karma and Sauce Labs demo',
+      testName: 'Adam Poczatek Blog',
       recordScreenshots: false,
       connectOptions: {
         port: 5757,
