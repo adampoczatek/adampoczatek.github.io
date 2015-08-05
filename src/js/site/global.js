83aba6fe794f98c3c7f8d9b9ac222c8f0eb27f17
Adam Poczatek <hello.adaz@gmail.com>
2015-08-05 22:22:26 +0100

    Contact form sorted

--- src/js/site/global.js
+++ src/js/site/global.js
@@ -1,13 +1,11 @@
 import {setupNav} from "modules/navigation.js";
 import {Modal} from "modules/modal.js";
 
-document.addEventListener("DOMContentLoaded", function () {
 
-    // Setup navigation.
-    setupNav();
+// Setup navigation.
+setupNav();
 
-    // Setup "Hire Me" modal.
-    new Modal(document.getElementById("hire-me"), 
-        document.getElementById("hire-me-close"), 
-        document.querySelectorAll(".hire-me-trigger"));
-});
+// Setup "Hire Me" modal.
+new Modal(document.getElementById("hire-me"), 
+    document.getElementById("hire-me-close"), 
+    document.querySelectorAll(".hire-me-trigger"));
