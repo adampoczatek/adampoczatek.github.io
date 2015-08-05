a23434907d2a707cd7c4865f09a18a4eafb6b667
Adam Poczatek <hello.adaz@gmail.com>
2015-08-05 22:58:45 +0100

    Cross-browser scrollTop solution

--- src/js/site/home.js
+++ src/js/site/home.js
@@ -25,7 +25,7 @@ window.setInterval(function () {
 	if (isScrolling) {
 		isScrolling = false;
 
-		let headerBottomPosition = window.scrollY + headerHeight;
+		let headerBottomPosition = getScrollTop() + headerHeight;
 
 		// Touches section 2
 		if (headerBottomPosition >= section2Position && 
@@ -43,6 +43,24 @@ window.setInterval(function () {
 		}
 	}
 }, 100);
+
+// Cross-browser scrollTop
+// http://stackoverflow.com/questions/871399/cross-browser-method-for-detecting-the-scrolltop-of-the-browser-window
+function getScrollTop(){
+    if (typeof pageYOffset !== "undefined") {
+
+        //most browsers except IE before #9
+        return pageYOffset;
+    }
+    else {
+        var B = document.body, //IE 'quirks'
+        	D = document.documentElement; //IE with doctype
+        
+        D = D.clientHeight ? D: B;
+        
+        return D.scrollTop;
+    }
+}
 
 function setScroll () {
 	isScrolling = true;
